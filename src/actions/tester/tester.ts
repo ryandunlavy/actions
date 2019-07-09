f750244fbf770cb8aeff3930205c7618da51cfa7
Ryan Dunlavy <ryandunlavy@Ryans-MacBook-Pro-3.local>
2019-07-09 16:52:22 -0700

    make dedicated tester action

---
+++ src/actions/tester/tester.ts
@@ -0,0 +1,26 @@
+import * as Hub from "../../hub"
+
+export class TesterAction extends Hub.Action {
+
+  requiredFields = []
+  description = "Test the full response of an action"
+  name = "tester"
+  label = "Tester"
+  params = []
+  supportedActionTypes = [Hub.ActionType.Query]
+  usesStreaming = true
+  supportedFormattings = [Hub.ActionFormatting.Unformatted]
+  supportedVisualizationFormattings = [Hub.ActionVisualizationFormatting.Noapply]
+  supportedFormats = [Hub.ActionFormat.JsonDetail]
+
+  async execute(request: Hub.ActionRequest) {
+    console.log(request)
+    return new Hub.ActionResponse({ success: true })
+  }
+
+  async form() {
+    const form = new Hub.ActionForm()
+    return form
+  }
+}
+Hub.addAction(new TesterAction())
