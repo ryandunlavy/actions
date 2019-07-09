f23e995303105c37a5b213639e22258ab8d40a0d
Ryan Dunlavy <ryandunlavy@Ryans-MacBook-Pro-3.local>
2019-07-09 14:30:28 -0700

    make tray a debugger

--- src/actions/tray/tray.ts
+++ src/actions/tray/tray.ts
@@ -1,14 +1,21 @@
 import * as Hub from "../../hub"
 
-import {WebhookAction} from "../webhook/webhook"
-
-export class TrayAction extends WebhookAction {
+export class TrayAction extends Hub.Action {
 
   name = "tray"
   label = "Tray"
   iconName = "tray/tray.svg"
   description = "Send data and begin a Tray workflow."
   domain = "trayapp.io"
+  usesStreaming = true
+  supportedFormats = [Hub.ActionFormat.JsonDetailLiteStream]
+  params = []
+  supportedActionTypes = [Hub.ActionType.Query]
+
+  async execute(request: Hub.ActionRequest) {
+    console.log(request)
+    return new Hub.ActionResponse({success: true})
+  }
 
   async form() {
     const form = new Hub.ActionForm()
@@ -20,7 +27,6 @@ export class TrayAction extends WebhookAction {
     }]
     return form
   }
-
 }
 
 Hub.addAction(new TrayAction())
