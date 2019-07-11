bb44f387fa56832696c4e4b5f7de9354e21c5e85
Ryan Dunlavy <ryandunlavy@Ryans-MacBook-Pro-3.local>
2019-07-11 09:27:23 -0700

    remove supported vis formats

--- src/actions/tester/tester.ts
+++ src/actions/tester/tester.ts
@@ -10,22 +10,20 @@ export class TesterAction extends Hub.Action {
   supportedActionTypes = [Hub.ActionType.Query]
   usesStreaming = true
   supportedFormattings = [Hub.ActionFormatting.Unformatted]
-  supportedVisualizationFormattings = [Hub.ActionVisualizationFormatting.Noapply]
+  //supportedVisualizationFormattings = [Hub.ActionVisualizationFormatting.Noapply]
   supportedFormats = [Hub.ActionFormat.JsonDetail]
 
   async execute(request: Hub.ActionRequest) {
-    console.log(request)
+    const fields = ""
     try {
       // let columns = errors.map((e) => e.message ? e.message : e).join(", ")
       await request.streamJsonDetail({
-        onFields: (fields) => {
-          console.log(fields)
-        },
         onRow: (row) => {
-          console.log(row)
+          row[1]
         },
       })
     } catch (e) {
+
     }
     return new Hub.ActionResponse({ success: true })
   }
